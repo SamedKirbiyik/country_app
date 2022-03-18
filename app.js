bed8942cb514c5259510f0fcc7bf3c77f7410a0c
SamedKirbiyik <samedkirbiyik@gmail.com>
2022-03-18 08:33:41 +0300

    add

---
+++ app.js
@@ -0,0 +1,27 @@
+// const data = fetch('https://restcountries.com/v3.1/name/Turkey');
+// console.log(data);
+
+// fetch('https://restcountries.com/v3.1/name/Turkey')
+//     .then(response => {
+//         console.log(response)
+//         return response.json();
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         console.log(data[0].capital, data[0].name.common);
+//     });
+
+const getCountry = async (countryName) => {
+    const response = await fetch(
+        `https://restcountries.com/v3.1/name/${countryName}`);
+    const data  = await response.json();
+    console.log(data);
+    console.log(data[0].capital, data[0].name.common);
+}
+getCountry('Turkey');
+
+const renderCountry = (data, type = 'country') => {
+    const flag = data.flags.svg;
+    const countryName = data.name.common;
+    
+}
